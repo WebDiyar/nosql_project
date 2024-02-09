3a7cc7345903cd1d9872759f6db8b386e7d04b8e
WebDiyar <diyar.atashka@mail.ru>
2024-02-09 17:27:04 +0600

    env

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const DB = `mongodb+srv://Diyar:Asd__200205_tr@diyar.qhyiil5.mongodb.net/mern_user`;
-const DB = process.env.DATABASE
+const DB = `mongodb+srv://Diyar:Asd__200205_tr@diyar.qhyiil5.mongodb.net/mern_user`;
 
 mongoose.connect(DB, {
     useUnifiedTopology: true,
